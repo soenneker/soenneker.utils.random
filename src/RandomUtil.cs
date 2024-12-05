aca0212206da91c18f59e70439721e402d1b7b1a
Jake Soenneker <jake@soenneker.com>
2024-12-05 16:25:09 -0600

    No longer supporting net6

--- src/RandomUtil.cs
+++ src/RandomUtil.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
-using System.Runtime.CompilerServices;
 
 namespace Soenneker.Utils.Random;
 
--- test/Soenneker.Utils.Random.Tests/RandomUtilTests.cs
+++ test/Soenneker.Utils.Random.Tests/RandomUtilTests.cs
@@ -1,5 +1,4 @@
 using FluentAssertions;
-using System;
 using System.Collections.Generic;
 using Xunit;
 
