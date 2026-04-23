a7708dbfcebcd76a958f1720da0d5b22f5125755
Jake Soenneker <jake@soenneker.com>
2026-04-22 19:23:43 -0500

    Tests

--- test/Soenneker.Utils.Random.Tests/RandomUtilTests.cs
+++ test/Soenneker.Utils.Random.Tests/RandomUtilTests.cs
@@ -5,10 +5,10 @@ namespace Soenneker.Utils.Random.Tests;
 
 public class RandomUtilTests
 {
-    [Theory]
-    [InlineData(2, 5)]
-    [InlineData(5.5, 7.2)]
-    [InlineData(7.54, 10.25)]
+    [Test]
+    [Arguments(2, 5)]
+    [Arguments(5.5, 7.2)]
+    [Arguments(7.54, 10.25)]
     public void NextDecimal_should_give_result_in_range(decimal min, decimal max)
     {
         decimal result = RandomUtil.NextDecimal(min, max);
@@ -16,10 +16,10 @@ public class RandomUtilTests
         result.Should().BeInRange(min, max);
     }
 
-    [Theory]
-    [InlineData(0, 0)]
-    [InlineData(5.5, 5.5)]
-    [InlineData(10.25, 10.25)]
+    [Test]
+    [Arguments(0, 0)]
+    [Arguments(5.5, 5.5)]
+    [Arguments(10.25, 10.25)]
     public void NextDecimal_should_give_input_result_if_equal(decimal min, decimal max)
     {
         decimal result = RandomUtil.NextDecimal(min, max);
@@ -27,10 +27,10 @@ public class RandomUtilTests
         result.Should().BeInRange(min, max);
     }
 
-    [Theory]
-    [InlineData(0, 0)]
-    [InlineData(5.5, 5.5)]
-    [InlineData(10.25, 10.25)]
+    [Test]
+    [Arguments(0, 0)]
+    [Arguments(5.5, 5.5)]
+    [Arguments(10.25, 10.25)]
     public void NextDecimalUniform_should_give_input_result_if_equal(decimal min, decimal max)
     {
         decimal result = RandomUtil.NextDecimalUniform(min, max);
@@ -38,10 +38,10 @@ public class RandomUtilTests
         result.Should().BeInRange(min, max);
     }
 
-    [Theory]
-    [InlineData(2, 5)]
-    [InlineData(5.5, 7.2)]
-    [InlineData(7.54, 10.25)]
+    [Test]
+    [Arguments(2, 5)]
+    [Arguments(5.5, 7.2)]
+    [Arguments(7.54, 10.25)]
     public void NextDecimalUniform_should_give_result_in_range(decimal min, decimal max) {
         decimal result = RandomUtil.NextDecimalUniform(min, max);
 
